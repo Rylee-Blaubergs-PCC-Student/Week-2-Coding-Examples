a9b2eef8b382b29052bffd38ed29931f7296bfca
Rylee-Blaubergs-PCC-Student <rylee.blaubergs@pcc.edu>
2026-02-10 04:14:18 +0000

    update

--- 2-9-26-WarmUp.cpp
+++ 2-9-26-WarmUp.cpp
@@ -24,12 +24,20 @@ int main() {
       return 1; // 1 indicates error
    }
 
+// get all data from txt file
    for (int i = 0; i < 5; i++) {
-    
-
-
+    inFS >> items[i], prices[i], amount[i];
+   }
+   
+// echo data from txt file
+   for (int i = 0; i < 5; i++) {
+   cout << "item: " << items[i] << ", ";
+   cout << "price: " << prices[i] << ", ";
+   cout << "amount: " << amount[i];
+   cout << endl;
    }
     
-    
+
+
 
 }
