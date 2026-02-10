e0a8afb4750a78dd28956d4afced202f567235d7
Rylee-Blaubergs-PCC-Student <rylee.blaubergs@pcc.edu>
2026-02-10 04:10:45 +0000

    update

--- 2-9-26-WarmUp.cpp
+++ 2-9-26-WarmUp.cpp
@@ -1,11 +1,34 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
+const int DATACAP = 32;
+const int MAXCHAR = 128;
 
 int main() {
+   char items[DATACAP][MAXCHAR];
+   double prices[DATACAP];
+   int amounts[DATACAP];
+
+
+
+    ifstream inFS;
+
+    // Try to open file
+   cout << "Opening file groceryList.txt." << endl;
+
+   inFS.open("groceryList.txt");
+   if (!inFS.is_open()) {
+      cout << "Could not open file groceryList.txt." << endl;
+      return 1; // 1 indicates error
+   }
+
+   for (int i = 0; i < 5; i++) {
     
-    
+
+
+   }
     
     
 
